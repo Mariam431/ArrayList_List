241cd27475c866d88a5a144909256c72ccbe41d0
mariam <mariam@MYPROBOOK>
2023-03-26 22:05:50 +0400

    Add project files.

---
+++ Homweork25.03/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace ArrayList._3
+{
+    using System;
+    using System.Collections;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var arlist1 = new ArrayList();                
+            var arlist2 = new ArrayList()
+                    {
+                        1, "Bill", " ", true, 4.5, null
+                    };
+                                       //to add an entire ARRAY, ARRAYLIST, HASHTABLE, SORTEDLIST, BITARRAY, QUEUE, STACK
+                                       // use arlist1.AddRange(arlist2);
+            arlist1.AddRange(arlist2); //to add an element in ArrayList use arlist1.Add("Hi");
+
+            foreach(var item in arlist1) // to print ArrayList (arlist1) use this code
+                Console.Write(item + ", ");
+
+            for (int i = 0; i < arlist2.Count; i++) // or to print ArrayList (arlist2) use this code
+                Console.Write(arlist2[i] + ", ");
+
+
+            
+
+            //insert an item in ArrayList
+            ArrayList arlist3 = new ArrayList()
+                {
+                    1,
+                    "Bill",
+                    300,
+                    4.5f
+                };
+
+            arlist3.Insert(1, "Second Item");
+
+            foreach (var val in arlist3)
+                Console.WriteLine(val);
+            //Use the InsertRange() method to insert a collection in an ArrayList at the specfied index.
+            ArrayList arlist5 = new ArrayList()
+                {
+                    100, 200, 600
+                };
+
+            ArrayList arlist6 = new ArrayList()
+                {
+                    300, 400, 500
+                };
+            arlist5.InsertRange(2, arlist6);
+
+            foreach (var item in arlist5)
+                Console.Write(item + ", ");
+
+            */
+
+            // Remove Elements from ArrayList
+            ArrayList arList = new ArrayList()
+                {
+                    1,
+                    null,
+                    "Bill",
+                    300,
+                    " ",
+                    4.5f,
+                    300,
+                };
+
+            arList.Remove(null); //Removes first occurance of null
+            arList.RemoveAt(4); //Removes element at index 4
+            arList.RemoveRange(0, 2);//Removes two elements starting from 1st item (0 index)
+            foreach (var b in arList)
+                Console.WriteLine(b + ", ");
+
+
+
+            //Check for Elements
+            //Use the Contains() method to determine whether the specified element exists in the ArrayList or not. It returns true if exists otherwise returns false.
+            ArrayList arList1 = new ArrayList()
+                {
+                    1,
+                    "Bill",
+                    300,
+                    4.5f,
+                    300
+                };
+
+            Console.WriteLine(arList1.Contains(300)); // true
+            Console.WriteLine(arList1.Contains("Bill")); // true
+            Console.WriteLine(arList1.Contains(10)); // false
+            Console.WriteLine(arList1.Contains("Steve")); // false
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> primeNumbers = new List<int>();
+            primeNumbers.Add(1); // adding elements using add() method
+            primeNumbers.Add(3);
+            primeNumbers.Add(5);
+            primeNumbers.Add(7);
+
+            var cities = new List<string>();
+            cities.Add("New York");
+            cities.Add("London");
+            cities.Add("Mumbai");
+            cities.Add("Chicago");
+            cities.Add(null);// nulls are allowed for reference type list
+
+            //adding elements using collection-initializer syntax
+            var bigCities = new List<string>()
+                    {
+                        "New York",
+                        "London",
+                        "Mumbai",
+                        "Chicago"
+                    };
+
+            // using foreach loop to print List
+            foreach(var c in cities)
+                Console.WriteLine(c + ", ");
+            // using for loop to print List
+            for (int i = 0; i < cities.Count; i++)
+                Console.WriteLine(cities[i]);
+
+
+
+
+            //Adding array to list
+            string[] cities1 = new string[3] { "Mumbai", "London", "New York" };
+
+            var popularCities = new List<string>();
+
+            // adding an array in a List
+            popularCities.AddRange(cities1);
+
+            var favouriteCities = new List<string>();
+
+            // adding a List 
+            favouriteCities.AddRange(popularCities);
+            foreach (var c in favouriteCities)
+                Console.WriteLine(c + ", ");
+
+            //Inserts new element into List
+            var numbers = new List<int>() { 10, 20, 30, 40 };
+
+            numbers.Insert(1, 11);// inserts 11 at 1st index: after 10.
+
+            foreach (var num in numbers)
+                Console.Write(num);
+
+
+            //remove elements from List
+            var numbers2 = new List<int>() { 10, 20, 30, 40, 10 };
+
+            numbers.Remove(10); // removes the first 10 from a list
+
+            numbers.RemoveAt(2); //removes the 3rd element (index starts from 0)
+
+            //numbers.RemoveAt(10); //throws ArgumentOutOfRangeException
+
+            foreach (var el in numbers2)
+                Console.Write(el); //prints 20 30
+
+
+            //Check elements in List
+            var numbers4 = new List<int>() { 10, 20, 30, 40 };
+            numbers4.Contains(10); // returns true
+            numbers4.Contains(11); // returns false
+            numbers4.Contains(20); // returns true
+
+
+
+
+
+
+
+
+        }
+    }
+}
